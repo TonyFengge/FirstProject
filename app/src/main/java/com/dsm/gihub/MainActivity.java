0c0b3e2a414774bd7db74cf6e52e9d5cd2d1c1aa
TonyFengge <1285193147@qq.com>
2016-11-22 16:36:42 +0800

    接着修改了下

--- app/src/main/java/com/dsm/gihub/MainActivity.java
+++ app/src/main/java/com/dsm/gihub/MainActivity.java
@@ -1,7 +1,7 @@
 package com.dsm.gihub;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,8 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //没有标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  // 去除标题栏
         setContentView(R.layout.activity_main);
     }
 }
