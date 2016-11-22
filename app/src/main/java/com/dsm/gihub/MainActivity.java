0b3ed0e8eea6f05712c937b155be15b1b8dda00a
TonyFengge <1285193147@qq.com>
2016-11-22 17:44:07 +0800

    接着来呗

--- app/src/main/java/com/dsm/gihub/MainActivity.java
+++ app/src/main/java/com/dsm/gihub/MainActivity.java
@@ -2,6 +2,7 @@ package com.dsm.gihub;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        View view = findViewById(R.id.activity_main);
     }
 }
