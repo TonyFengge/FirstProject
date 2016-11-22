0a6153d52724627549d053186a5cb6cbbcaca784
TonyFengge <1285193147@qq.com>
2016-11-22 16:32:07 +0800

    加了一点东西

--- app/src/main/java/com/dsm/gihub/MainActivity.java
+++ app/src/main/java/com/dsm/gihub/MainActivity.java
@@ -3,14 +3,21 @@ package com.dsm.gihub;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //没有标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        TextView textView = (TextView) findViewById(R.id.tv_content);
+        textView.setText("This is a branch");
     }
 }
